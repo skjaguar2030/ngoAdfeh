11766ceefdf6566a849ca10864b1984d5a76d310
skjaguar2030 <seifira21@protonmail.com>
2022-11-02 20:25:44 +0200

    I tweaked a bit my code and added some new details

--- js/navbar.js
+++ js/navbar.js
@@ -3,10 +3,13 @@ window.onscroll = function() {myFunction()};
 var myHeader = document.getElementById("my-header");
 var sticky = myHeader.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
+function myFunction() {  // Before I used pageYOffset
+  if (window.scrollY >= sticky) {
     myHeader.classList.add("sticky")
+    // myHeader.classList.add("boxShadow")
+
   } else {
     myHeader.classList.remove("sticky");
+    // myHeader.classList.remove("boxShadow")
   }
 }
