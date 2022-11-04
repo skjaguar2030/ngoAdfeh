7fe951f5d7803caebf82d8d71c298ed9b3f64a8d
skjaguar2030 <seifira21@protonmail.com>
2022-11-04 17:07:11 +0200

    navigation bar working as expecting. Colors not yet setted. Still some details to fix in the mobile version

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,12 @@
-window.onscroll = function() {myFunction()};
 
-var myHeader = document.getElementById("my-header");
-var sticky = myHeader.offsetTop;
 
-function myFunction() {  // Before I used pageYOffset
-  if (window.scrollY >= sticky) {
-    myHeader.classList.add("sticky")
-    // myHeader.classList.add("boxShadow")
-
+window.onscroll = function() {stickyNav()};
+var mynavbar = document.getElementById("my-header");
+var sticky = mynavbar.offsetTop;
+function stickyNav() {
+  if (window.pageYOffset > sticky) {
+    mynavbar.classList.add("fixed");
   } else {
-    myHeader.classList.remove("sticky");
-    // myHeader.classList.remove("boxShadow")
+    mynavbar.classList.remove("fixed");
   }
-}
+} 
